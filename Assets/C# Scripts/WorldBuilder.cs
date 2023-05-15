1701fee25a594996c5650b8c56519ab72545f804
Nicolas K <57925500+HEL3X@users.noreply.github.com>
2023-05-15 11:39:45 -0700

    Maze Gen v.1 + PlayerMove
    Currently Maze Gen has error that causes crash. But the generation itself looks good. The only problem is that there's a very small chance that the player can be blocked off.

--- Assets/C# Scripts/WorldBuilder.cs
+++ Assets/C# Scripts/WorldBuilder.cs
@@ -1,31 +1,106 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
-    [SerializeField] public GameObject mazePrefab;
+    public int width = 10;  // Width of the maze (number of cells)
+    public int height = 10; // Height of the maze (number of cells)
+    public float cellSize = 1f; // Size of each cell
+
+    private Cell[,] cells; // 2D array to store the cells
+
+    // Cell class to represent each cell in the maze
+    private class Cell
+    {
+        public bool visited;
+        public GameObject northWall;
+        public GameObject eastWall;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 100; i+=5)
+        GenerateMaze();
+    }
+
+    // Generate the maze using the Binary Tree algorithm
+    void GenerateMaze()
+    {
+        // Initialize the cell grid
+        cells = new Cell[width, height];
+        for (int x = 0; x < width; x++)
         {
-            for (int j = 0; j < 100; j+=5)
+            for (int y = 0; y < height; y++)
             {
-                Vector3 pos = new Vector3(i, 3, j);
-                if (pos != new Vector3(0, 3, 0) && pos != new Vector3(95, 3, 95))
-                {
-                    Instantiate(mazePrefab, pos, Quaternion.identity);
-                }
-                
+                cells[x, y] = new Cell();
             }
         }
 
+        // Create the maze layout
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Create walls for each cell
+                cells[x, y].northWall = CreateNorthWall(x, y, x + 1, y);
+                cells[x, y].eastWall = CreateEastWall(x + 1, y, x + 1, y + 1);
+  
+            }
+        }
+
+        // Randomly remove walls using Binary Tree algorithm
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // List of possible directions to remove a wall
+                List<int> directions = new List<int>();
+
+                // Add north direction if within bounds
+                if (y < height - 1)
+                {
+                    directions.Add(0);
+                }
+
+                // Add east direction if within bounds
+                if (x < width - 1)
+                {
+                    directions.Add(1);
+                }
+
+                // Choose a random direction from the available directions
+                int direction = directions[Random.Range(0, directions.Count)];
+
+                // Remove the wall in the chosen direction
+                if (direction == 0)
+                {
+                    Destroy(cells[x, y].northWall);
+                }
+                else if (direction == 1)
+                {
+                    Destroy(cells[x, y].eastWall);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Create a wall between two cells
+    GameObject CreateNorthWall(int x1, int y1, int x2, int y2)
     {
-        
+        Vector3 position = new Vector3((x1 + x2) * cellSize * 0.5f, 0f, (y1 + y2) * cellSize * 0.5f);
+        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.transform.position = position;
+        wall.transform.localScale = new Vector3(cellSize, cellSize * 2f, 1f);
+        return wall;
+    }
+
+    GameObject CreateEastWall(int x1, int y1, int x2, int y2)
+    {
+        Vector3 position = new Vector3((y1 + y2) * cellSize * 0.5f, 0f, (x1 + x2) * cellSize * 0.5f);
+        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.transform.position = position;
+        wall.transform.localScale = new Vector3(1f, cellSize * 2f, cellSize);
+        return wall;
     }
 }
